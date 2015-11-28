ba7d409d85c90a70a4c73b2bb6f00dab73489946
geordanex <geordan62@gmail.com>
2015-11-28 04:43:03 -0500

    Commit Screen Creditos

---
+++ Assets/scripts/CreditsController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(0f, 40f, 0f) * Time.deltaTime;
+	}
+}
