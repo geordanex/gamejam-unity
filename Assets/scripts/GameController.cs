fa4827f1229150e0361eccb959a7fbc0f51c9d81
geordanex <geordan62@gmail.com>
2015-11-28 16:50:33 -0500

    Commit UI Contador

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
+    public float count = 0;
+    public string Text;
+    //public Component Count;
+    public GameObject texto;
+
+    private Text contador;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        contador = texto.GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        count += Time.deltaTime;
+        contador.text = count.ToString();
 	}
+   
 }
